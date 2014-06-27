726522de0b06daa3b170b7b299bb1f097b43f89a
aleramirez <aleramirez44@hotmail.com>
2014-06-27 16:17:01 -0500

    Primer Commit

---
+++ practica1/paginas web/guardar.php
@@ -0,0 +1,23 @@
+<?php
+	
+	echo "Goles Equipo Local 1: ".$_POST['equipo1_local'];
+	echo "<br>";
+	echo "Goles Equipo Visitante 1: ".$_POST['equipo2_visitante'];
+	echo "<br>";
+	
+	echo "Goles Equipo Local 2: ".$_POST['equipo3_local'];
+	echo "<br>";
+	echo "Goles Equipo Visitante 2: ".$_POST['equipo4_visitante'];
+	echo "<br>";
+	
+	echo "Equipo elegido local: ".$_POST['visitante']."<br>";
+	echo "Goles Equipo Local 3: ".$_POST['equipo5_local'];
+	echo "<br>";
+	echo "Equipo elegido visitante: ".$_POST['local']."<br>";
+	echo "Goles Equipo Visitante 3: ".$_POST['equipo6_visitante'];
+	echo "<br>";
+	
+	
+	
+
+?>
