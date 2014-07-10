97ce803b24faea84993d8d6e015ab0aa35de7570
aleramirez <aleramirez44@hotmail.com>
2014-07-09 23:45:48 -0500

    avanceproyecto1

---
+++ proyecto1.1/proyecto1/views/layouts/footer.php
@@ -0,0 +1,31 @@
+ 
+	 <div class="row">
+	 	
+	 	<div class="col-md-12">
+	 		<hr>
+	 		<p class="text-center">
+	 			
+	 			Clase programación web. <br><span class="glyphicon glyphicon-registration-mark"> </span> <?php echo date('Y'); ?>
+	 			
+	 		</p>
+	 	</div>	
+	 	
+	 </div>
+
+ </div> <!-- /container -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Include Raty JS file -->
+	<script src="js/jquery.raty.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/jquery-1.10.2.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>  
+	 <script type="text/javascript" src="js/moment.js"></script>
+    <script type="text/javascript" src="js/bootstrap-datetimepicker.js"></script>
+    <script type="text/javascript" src="js/bootstrap-datetimepicker.es.js"></script>
+    <script  type = "text/javascript"  src = "js/jquery.fancybox.pack.js" ></script>
+
+  </body>
+</html>
---
+++ proyecto1.1/proyecto1/views/site/registro_estadio.php
@@ -0,0 +1,44 @@
+<html>
+	<head>
+
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <title>Bootstrap 101 Template</title>
+
+	    <!-- Bootstrap -->
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<title>Registro Estadio</title>
+		<!-- <link rel="stylesheet" type="text/css" href="css/estilo.css" />-->
+	</head>
+	
+	<body>
+
+		<h2 class="text-center">Registro del Estadio:</h2><hr>
+		<form role="form">
+			  <div class="form-group">
+			    <label for="Nombre">Nombre:</label>
+			    <input type="text" class="form-control" id="Nombre" placeholder="Enter nombre">
+			  </div>
+
+			   <div class="form-group">
+			  	
+			  		<label for="idciudad">Identificador de la Ciudad:</label>
+			  		<select class="form-control">
+						  <option>Elige uno:</option>
+						  <option>1</option>
+						  <option>2</option>
+						  <option>3</option>
+						  <option>4</option>
+						  <option>5</option>
+				</select>
+			</div>
+
+  			<button type="button" class="btn btn-primary">Guardar</button>
+
+		</form>
+ 
+	</body>
+
+</html>
